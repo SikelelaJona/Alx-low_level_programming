fb808a87759752685be9c5d95bf744ecc8e384be
sikelela jona <48279605+SikelelaJona@users.noreply.github.com>
2022-03-12 06:10:50 +0200

    positive_negative_zero_numbers

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,1 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+  
+  //int n;
+    
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%d", &n);
+    
+
+    if(n > 0)
+    {
+        printf("Number is POSITIVE");
+    }
+    if(n < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    if(n == 0)
+    {
+        printf("Number is ZERO");
+    }
+	return (0);
+}
